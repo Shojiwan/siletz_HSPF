d371ff9f780255f1662507ada5c46e666d04239c
Shojiwan <74939573+Shojiwan@users.noreply.github.com>
2022-02-11 11:41:01 +1000

    Updated build functions

--- python/ctrl_build_hspf.py
+++ python/ctrl_build_hspf.py
@@ -1,28 +1,39 @@
 # Main control file for building HSPF model files
-
-import os, csv, pickle, datetime, math
+import os, csv, pickle, datetime
+import math as math
 import pandas as pd
 import shapefile
 from pyhspf import Subbasin, Watershed, HSPFModel, WDMUtil
 from array import *
 
+dir1  = 'G:/005_model/001_SLZ/'
+
 # Source the functions for processing and builing model
-exec(open('c:/siletz_tmdl/04_scripts/01_hspf/01_python/fnct_build_hspf.py').read())
+exec(open(dir1 + '04_scripts/01_hspf/01_python/fnct_build_hspf.py').read())
+
+strD = datetime.datetime(2004, 1, 1)
+endD = datetime.datetime(2018, 1, 1)
 
 # Specify the land cover (HRU) file and path
-hruF = 'C:/siletz_tmdl/01_inputs/01_hspf/lulc/hru_2011.csv'
+hruF = dir1 + '01_inputs/01_hspf/lulc/hru_2011.csv'
+
 # Specify the precipitation file and path
-pcpF = 'C:/siletz_tmdl/01_inputs/01_hspf/pcp/pcp_base.csv'
+pcpF = dir1 + '01_inputs/01_hspf/pcp/pcp_base.csv'
+
 # Specify the PET file and path
-petF = 'C:/siletz_tmdl/01_inputs/01_hspf/pet/pet_base.csv'
+petF = dir1 + '01_inputs/01_hspf/pet/pet_base.csv'
+
 # Specify the fTable file and path
-fTab = ''
+fTab = dir1 + '01_inputs/01_hspf/ftab/ftab.csv'
+
 # Specify the basin shapefile and path
+bsnF = dir1 + '01_inputs/01_hspf/shp/basins.shp'
 
 # Specify the reach shapefile and path
+rchF = dir1 + '01_inputs/01_hspf/shp/reaches.shp'
 
 # Specify the model file and path (where to write the model)
-modF = 'C:/siletz_tmdl/03_models/01_hspf/siletz.hspf'
+modF = dir1 + '03_models/01_hspf/siletz.hspf'
 
 # BUILD THE MODEL
-build_hspf_model(hruF, pcpF, petF, fTab, modF)
+build_hspf_model(hruF, pcpF, petF, fTab, bsnF, rchF, modF)
